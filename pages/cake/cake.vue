18b78dc853cdd1b125d5266715f8a12e478a05f9
AA12GQ <875151567@qq.com>
2022-04-10 13:45:09 +0800

    初始化项目-商品列表页面布局实现

---
+++ pages/cake/cake.vue
@@ -0,0 +1,66 @@
+<template>
+	<view>
+		<view class="count">
+			<view class="cake-item" v-for="(item,index) in 8">
+				<view class="poster">
+					
+				</view>
+				<view class="info-cont">
+					<view class="info">
+						<view class="fs-28">
+							草莓蛋糕
+						</view>
+						<view class="fs-16">
+							CaoMei DanGao
+						</view>
+						<view class="fs-18">
+							<text class="fs-14">￥</text>
+							218.00
+						</view>
+					</view>
+					<view class="cart-btn">
+						
+					</view>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style lang="scss">
+.count{
+	display: flex;
+	flex-wrap: wrap;
+	padding: 15upx;
+	justify-content: space-between;
+}
+.cake-item{
+	width: 350upx;
+	.poster{
+		height: 350upx;
+		background-color: #f5f5f5;
+	}
+	.fs-28{
+		margin-top: 26upx;
+	}
+	.fs-16{
+		margin:14upx 0;
+	}
+	.fs-14{
+		margin-bottom: 22upx;
+	}
+}
+</style>
---
+++ unpackage/dist/dev/mp-weixin/pages/classify/classify.js
@@ -0,0 +1,31 @@
+(global["webpackJsonp"] = global["webpackJsonp"] || []).push([["pages/classify/classify"],{
+
+/***/ 28:
+/*!************************************************************************************!*\
+  !*** G:/HBuilderProjects/cust_cake/main.js?{"page":"pages%2Fclassify%2Fclassify"} ***!
+  \************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+/* WEBPACK VAR INJECTION */(function(createPage) {__webpack_require__(/*! uni-pages */ 5);
+var _vue = _interopRequireDefault(__webpack_require__(/*! vue */ 3));
+var _classify = _interopRequireDefault(__webpack_require__(/*! ./pages/classify/classify.vue */ 29));function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}wx.__webpack_require_UNI_MP_PLUGIN__ = __webpack_require__;
+createPage(_classify.default);
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! ./node_modules/@dcloudio/uni-mp-weixin/dist/index.js */ 1)["createPage"]))
+
+/***/ }),
+
+/***/ 29:
+/*!*****************************************************************!*\
+  !*** G:/HBuilderProjects/cust_cake/pages/classify/classify.vue ***!
+  \*****************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed (from ./node_modules/@dcloudio/webpack-uni-mp-loader/lib/style.js):\nError: ENOENT: no such file or directory, open 'G:\\HBuilderProjects\\cust_cake\\pages\\classify\\classify.vue'");
+
+/***/ })
+
+},[[28,"common/runtime","common/vendor"]]]);
+//# sourceMappingURL=../../../.sourcemap/mp-weixin/pages/classify/classify.js.map
