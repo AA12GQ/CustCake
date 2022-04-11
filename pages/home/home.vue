09db2efcc03f100caf28cd6b593fcd25c8cfc8ff
AA12GQ <875151567@qq.com>
2022-04-11 23:34:48 +0800

    返回顶部功能实现

--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,48 +1,63 @@
 <template>
-	<view>
-		<nav-custom></nav-custom>
-		<swiper class="banner" :indicator-dots="true" :autoplay="true" indicator-active-color="#ffe32a">
-			<swiper-item v-for="(item,index) in 2">
-				<view class="swiper-item">
-					<image src="../../static/img/banner01.jpg" mode=""></image>
+	<scroll-view scroll-with-animation :scroll-into-view="topItem" class="scroll-cont" scroll-y="true" @scroll="handelScroll">
+		<view>
+			<view id="top"></view>
+			<nav-custom></nav-custom>
+			<swiper class="banner" :indicator-dots="true" :autoplay="true" indicator-active-color="#ffe32a">
+				<swiper-item v-for="(item,index) in 2">
+					<view class="swiper-item">
+						<image src="../../static/img/banner01.jpg" mode=""></image>
+					</view>
+				</swiper-item>
+			</swiper>
+			<home-title
+			title="本季推荐"
+			en-title="Seasonal Recommend"
+			en-tit="Seasonal"
+			>
+			</home-title>
+			<scroll-view scroll-x="true" >
+				<view class="scroll-inner">
+					<image src="../../static/img/recommend01.jpg" mode="heightFix"></image>
+					<image src="../../static/img/recommend02.jpg" mode="heightFix"></image>
+					<image src="../../static/img/recommend03.jpg" mode="heightFix"></image>
 				</view>
-			</swiper-item>
-		</swiper>
-		<home-title
-		title="本季推荐"
-		en-title="Seasonal Recommend"
-		en-tit="Seasonal"
-		>
-		</home-title>
-		<scroll-view scroll-x="true" >
-			<view class="scroll-inner">
-				<image src="../../static/img/recommend01.jpg" mode="heightFix"></image>
-				<image src="../../static/img/recommend02.jpg" mode="heightFix"></image>
-				<image src="../../static/img/recommend03.jpg" mode="heightFix"></image>
+			</scroll-view>
+			<home-title
+			title="法式经典"
+			en-title="French Classics"
+			en-tit="French"
+			>
+			</home-title>
+			<image class="classify" src="../../static/img/classify.jpg" mode=""></image>
+			<view class="flex flex-wrap justify-between padding-sm">
+				<good-item v-for="(item,index) in 4"></good-item>
 			</view>
-		</scroll-view>
-		<home-title
-		title="法式经典"
-		en-title="French Classics"
-		en-tit="French"
-		>
-		</home-title>
-		<image class="classify" src="../../static/img/classify.jpg" mode=""></image>
-		<view class="flex flex-wrap justify-between padding-sm">
-			<good-item v-for="(item,index) in 4"></good-item>
+			<view class="back-top" v-if="isShow" @click="handelBackUp">
+				<text class="iconfont icon-tubiao_fanhuidingbu"></text>
+			</view>
 		</view>
-	</view>
+	</scroll-view>
 </template>
 
 <script>
 	export default {
 		data() {
 			return {
-				
+			isShow : false,
+			topItem:''
 			}
 		},
 		methods: {
-			
+			handelScroll(ev){
+				console.log(ev)
+				let {scrollTop} = ev.detail
+				this.isShow = scrollTop>500
+				this.topItem = ''
+			},
+			handelBackUp(){
+				this.topItem = 'top'
+			}
 		}
 	}
 </script>
@@ -71,4 +86,20 @@
 	height: 380upx;
 	width:  100%;
 }	
+.back-top{
+	width:100upx;
+	height: 100upx;
+	background-color: #fff;
+	border-radius: 50%;
+	box-shadow: 0 0 30upx 4upx rgba(0,0,0,0.4);
+	position: fixed;
+	bottom: 40upx;
+	right: 20upx;
+	text-align: center;
+	line-height: 100upx;
+}
+.scroll-cont{
+	height: 100vh;
+	
+}
 </style>
